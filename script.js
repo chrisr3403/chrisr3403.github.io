9e5067a249f88d2f6afdc2a4116a44df459e7488
chrisr3403 <135894959+chrisr3403@users.noreply.github.com>
2024-06-09 19:10:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
+document.querySelector("#github").href = "http://github.com/" + chrisr3403;
+document.querySelector("#fork").href = "http://github.com/" + chrisr3403 + "/" + chrisr3403 + ".github.io";
